245c2253a387a165bb64bbc349649f96ce5754b0
wilmar mac <wilmar.sepulveda@correounivalle.edu.co>
2024-05-30 15:18:18 -0500

    fix for univalle ip

--- src/utils.py
+++ src/utils.py
@@ -32,8 +32,12 @@ def client_ip():
     except: return None
 
 def get_info_from_ip():
-    ip = client_ip()
     token = os.environ['IP_INFO_TOKEN']
-    ipapi_url = f"https://ipinfo.io/{ip}?token={token}"
+    ip = client_ip()
+    if ip is None:
+        new_ip = '45.5.167.153'
+        ipapi_url = f"https://ipinfo.io/{new_ip}?token={token}"
+    else: 
+        ipapi_url = f"https://ipinfo.io/{ip}?token={token}"
     response_json = requests.get(ipapi_url).json()
     return response_json
